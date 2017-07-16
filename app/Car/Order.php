4955669f2072829709383fbe4d1575a57fc40e7c
stivenson <stivenson.rpm@gmail.com>
2017-07-16 13:31:06 -0500

    Save orders

--- app/Car/Order.php
+++ app/Car/Order.php
@@ -6,80 +6,82 @@ use Models\ItemsOrder;
 
 class Order extends Resource{
 
-	public function listTypeDelivery($type = 0) {
-		
-		$items = [];
-		$type = (int)$type;
-		if($type == 0)
-			$items =  MOrder::all();
-		else
-			$items = MOrder::where('delivery_type',$type)->get();
-			
-		return $items;
-	}
-	
-	public function listStatus($type = 0) {
-	
-		$items = [];
-		$type = (int)$type;
-		if($type == 0)
-			$items =  MOrder::all();
-		else
-			$items = MOrder::where('status',$type)->get();
-					
-		return $items;
-	}
-	
-	public function find($id) {
-		$o = MOrder::find($id);
-		return !is_null($o) ? $o : false;
-	}
-	
-	/*
-	public function save($attr) {
-		$o = new MOrder();
-		$o->fill($attr);
-		return $o->save() ? $o: false;
-	}
-	*/
-	
-	public function save($attr) {
-		$itemsOrder = $attr['items_orders'];
-		$o = new MOrder();
-		$o->fill($attr);
-		return $this->transactionOrderAndItems($o, $itemsOrder);
-	}
-	
-	private function transactionOrderAndItems($o, $itemsOrder){
-		try {
-			\DB::transaction(function() use ($o, $itemsOrder)
-			{
-				$o->save();
-				foreach ($itemsOrder as $itemorder){
-					$io = new ItemsOrder;
-					$io->products_id = $itemorder->products_id;
-					$io->amount = $itemorder->amount;
-					$io->observations = $itemorder->observations;
-					$io->orders_id = $o->id; 
-					$io->save();
-				}
-			});
-			return true;
-		}catch (Exception $e){
-			return false;
-		}
-	}
-	
-	public function update($id,$attr) {
-		$o = MOrder::find($id);
-		$o->fill($attr);
-		return $o->save() ? $o: false;
-	}
-	
-	public function delete($id) {
-		$o = MOrder::find($id);
-		$o->delete();
-	}
+    public function listTypeDelivery($type = 0) {
+        
+        $items = [];
+        $type = (int)$type;
+        if($type == 0)
+            $items =  MOrder::all();
+        else
+            $items = MOrder::where('delivery_type',$type)->get();
+            
+        return $items;
+    }
+    
+    public function listStatus($type = 0) {
+    
+        $items = [];
+        $type = (int)$type;
+        if($type == 0)
+            $items =  MOrder::all();
+        else
+            $items = MOrder::where('status',$type)->get();
+                    
+        return $items;
+    }
+    
+    public function find($id) {
+        $o = MOrder::find($id);
+        return !is_null($o) ? $o : false;
+    }
+    
+    /*
+    public function save($attr) {
+        $o = new MOrder();
+        $o->fill($attr);
+        return $o->save() ? $o: false;
+    }
+    */
+    
+    public function save($attr) {
+        //var_dump($attr);
+        $itemsOrder = $attr['items_orders'];
+        $o = new MOrder();
+        $o->fill($attr);
+        return $this->transactionOrderAndItems($o, $itemsOrder);
+    }
+    
+    private function transactionOrderAndItems($o, $itemsOrder){
+        try {
+            \DB::transaction(function() use ($o, $itemsOrder)
+            {
+                $o->save();
+                $arrJson = json_decode($itemsOrder,true);
+                foreach ($arrJson as $itemorder){
+                    $io = new ItemsOrder;
+                    $io->products_id = $itemorder['products_id'];
+                    $io->amount = $itemorder['amount'];
+                    $io->observations = $itemorder['observations'];
+                    $io->orders_id = $o->id;
+                    $io->save();
+                }
+            });
+            return true;
+        }catch (Exception $e){
+            return false;
+        }
+    }
+    
+    public function update($id,$attr) {
+        $o = MOrder::find($id);
+        $o->fill($attr);
+        return $o->save() ? $o: false;
+    }
+    
+    public function delete($id) {
+        $o = MOrder::find($id);
+        $o->delete();
+    }
 }
 
 
--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -7,92 +7,80 @@ use Factories\Car;
 
 class OrderController extends Controller
 {
-	/**
-	 * Display a listing of the resource.
-	 *
-	 * @return \Illuminate\Http\Response
-	 */
-	public function index()
-	{
-		return response()->json(Car::getOrder()->listStatus(), 200);
-	}
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return response()->json(Car::getOrder()->listStatus(), 200);
+    }
 
-	/**
-	 * Display a listing of the resource with filter of type_delivery.
-	 *
-	 * @return \Illuminate\Http\Response
-	 */
-	public function indexTypeDelivery($type)
-	{
-		return response()->json(Car::getOrder()->listTypeDelivery($type), 200);
-	}
-	
-	/**
-	 * Display a listing of the resource with filter of status.
-	 *
-	 * @return \Illuminate\Http\Response
-	 */
-	public function indexStatus($type)
-	{
-		return response()->json(Car::getOrder()->listStatus($type), 200);
-	}
-	
-	/**
-	 * Display the specified resource.
-	 *
-	 * @param  int  $id
-	 * @return \Illuminate\Http\Response
-	 */
-	public function show($client)
-	{
-		return response()->json(Car::getOrder()->find($client), 200);
-	}
-	
-	/**
-	 * Store a newly created resource in storage.
-	 *
-	 * @param  \Illuminate\Http\Request  $request
-	 * @return \Illuminate\Http\Response
-	 */
-	public function store()
-	{
-		$inputs = $this->request->all();
-		return response()->json(Car::getOrder()->save($inputs), 200);
-	}
-	
-	/**
-	 * storeWithNestedOrders a newly created resource in storage with nested resources.
-	 *
-	 * @param  \Illuminate\Http\Request  $request
-	 * @return \Illuminate\Http\Response
-	 */
-	public function storeWithNestedOrders()
-	{
-		$inputs = $this->request->all();
-		return response()->json(Car::getOrder()->saveWithNestedOrders($inputs), 200);
-	}
-	
-	/**
-	 * Update the specified resource in storage.
-	 *
-	 * @param  \Illuminate\Http\Request  $request
-	 * @param  int  $id
-	 * @return \Illuminate\Http\Response
-	 */
-	public function update($client)
-	{
-		$inputs = $this->request->all();
-		return response()->json(Car::getOrder()->update($client,$inputs), 200);
-	}
-	
-	/**
-	 * Remove the specified resource from storage.
-	 *
-	 * @param  int  $id
-	 * @return \Illuminate\Http\Response
-	 */
-	public function destroy($client)
-	{
-		return response()->json(Car::getOrder()->delete($client), 200);
-	}
+    /**
+     * Display a listing of the resource with filter of type_delivery.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function indexTypeDelivery($type)
+    {
+        return response()->json(Car::getOrder()->listTypeDelivery($type), 200);
+    }
+    
+    /**
+     * Display a listing of the resource with filter of status.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function indexStatus($type)
+    {
+        return response()->json(Car::getOrder()->listStatus($type), 200);
+    }
+    
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($client)
+    {
+        return response()->json(Car::getOrder()->find($client), 200);
+    }
+    
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store()
+    {
+        $inputs = $this->request->all();
+        return response()->json(Car::getOrder()->save($inputs), 200);
+    }
+    
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update($client)
+    {
+        $inputs = $this->request->all();
+        return response()->json(Car::getOrder()->update($client,$inputs), 200);
+    }
+    
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($client)
+    {
+        return response()->json(Car::getOrder()->delete($client), 200);
+    }
 }
